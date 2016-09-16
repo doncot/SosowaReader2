699ecdcd1a7bfd6257a64f944e4778f7963cdfa5
doncot <doncot@gmail.com>
2016-09-17 00:41:09 +0900

    プロジェクト ファイルを追加します。

---
+++ SosowaReader/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+using Prism.Unity.Windows;
+using Prism.Windows;
+
+namespace SosowaReader
+{
+    /// <summary>
+    /// 既定の Application クラスを補完するアプリケーション固有の動作を提供します。
+    /// </summary>
+    sealed partial class App : PrismApplication
+    {
+        /// <summary>
+        /// 単一アプリケーション オブジェクトを初期化します。これは、実行される作成したコードの
+        ///最初の行であるため、main() または WinMain() と論理的に等価です。
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
+        {
+            NavigationService.Navigate("Main", null);
+
+            return Task.FromResult<object>(null);
+        }
+
+        /// <summary>
+        /// We use this method to simulate the loading of resources from different sources asynchronously.
+        /// </summary>
+        /// <returns></returns>
+        private Task LoadAppResources()
+        {
+            return Task.Delay(100);
+        }
+    }
+}
