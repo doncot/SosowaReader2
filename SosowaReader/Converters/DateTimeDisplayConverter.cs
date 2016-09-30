61df1bde37639fcdc55f9f2507cb2a2d4ca61b8e
doncot <doncot@gmail.com>
2016-10-01 01:25:23 +0900

    日付の表示をスマートに。

---
+++ SosowaReader/Converters/DateTimeDisplayConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace SosowaReader.Converters
+{
+    public class DateTimeDisplayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var date = (DateTime)value;
+            var test = DateTime.Today;
+            if (date.Date == DateTime.Today.Date)
+            {
+                //今日分は時分のみ
+                return $"{date.Hour}:{date.Minute}"; 
+            }
+
+            //同年は年を抜く
+            if(date.Year == DateTime.Today.Year)
+            {
+                return date.ToString("M/d H:m");
+            }
+
+            //デフォルト
+            return date.ToString("yy/mMd H:m");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
