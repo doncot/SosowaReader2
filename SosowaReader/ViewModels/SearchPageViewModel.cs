07c7f3ad98dab3691a3890498d177c7e3d095088
doncot <doncot@gmail.com>
2016-12-22 09:06:15 +0900

    検索VMを追加

---
+++ SosowaReader/ViewModels/SearchPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Commands;
+using Prism.Windows.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SosowaReader.ViewModels
+{
+    public class SearchPageViewModel : ViewModelBase
+    {
+        private DelegateCommand searchCommand;
+        public DelegateCommand SearchCommand
+        {
+            set { searchCommand = value; }
+            get
+            {
+                return DelegateCommand.FromAsyncHandler(SearchAsync);
+            }
+        }
+
+        public async Task SearchAsync()
+        {
+
+        }
+    }
+}
